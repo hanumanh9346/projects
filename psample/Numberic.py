448ea9d51a91cc348b712c1d2877ca64354d4cdf
hanuman9346 <hanumanh9346@gmail.com>
2018-11-25 14:55:39 -0500

    adding python projects

---
+++ psample/Numberic.py
@@ -0,0 +1,12 @@
+print 1 to 10
+
+print 1 to 100
+
+print even numbers 1 to 10
+
+ print odd numbers 1 to 10
+
+print  even numbers 1 to 100
+
+
+print odd numbers 1 to 100
---
+++ psample/print.py
@@ -0,0 +1,23 @@
+
+# a = "something"
+# for i in range(0, len(a),):
+#     print(a[i])
+    # if i%2 == 0:
+    #     print(i)
+
+
+
+# name = int(input("Enter your name"))
+# print("Hellooo" + name)
+
+#
+# a = "5"
+# print(type(a))
+# b = float(a)
+# print(type(b))
+# c = str(b)
+# print(type(c))
+
+
+a = 'Hello,World'
+print(a)
