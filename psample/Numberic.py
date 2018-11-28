09c3545a2e71581d520ca43670001fef0094dbe0
hanuman9346 <hanumanh9346@gmail.com>
2018-11-27 22:06:02 -0500

    added addition function

--- psample/Numberic.py
+++ psample/Numberic.py
@@ -1,12 +1,24 @@
-print 1 to 10
+# print 1 to 10
+#
+# print 1 to 100
+#
+# print even numbers 1 to 10
+#
+#  print odd numbers 1 to 10
+#
+# print  even numbers 1 to 100
+#
+#
+# print odd numbers 1 to 100
 
-print 1 to 100
-
-print even numbers 1 to 10
-
- print odd numbers 1 to 10
-
-print  even numbers 1 to 100
-
-
-print odd numbers 1 to 100
+# for i in range(0, 10,):
+#  if i%2 == 0:
+#       print(i)
+#
+# for i in range(1,11):
+#   print(i)
+#
+a= range(1,101)
+for i in a:
+ print(i)
+# print(a)
---
+++ psample/functions.py
@@ -0,0 +1,16 @@
+# my function started here
+def my_function(name):
+  fullname = name + "bala"
+  return fullname
+
+result_fun = my_function("Hanuman - ")
+print (result_fun)
+
+def add(a,b):
+  c = a+b
+  return c
+
+addition = add(150,50)
+print("addition of 2 numbers a, b is  ",addition)
+
+
